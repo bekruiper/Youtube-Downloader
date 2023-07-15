40f34c6199cb18d5b380fec09b3b44f40d553f5b
bekruiper <119519973+bekruiper@users.noreply.github.com>
2023-07-15 19:53:31 -0300

    Youtube-Downloader v.1.2.py
    - No fetched results limit. 
    - Small checkbox besides the thumbnails.
    - Button to select all the links in the page.
    - Two buttons : download all the videos, and download the videos selected with the checkbox.

--- Youtube-Downloader.py
+++ Youtube-Downloader.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from PIL import ImageTk, Image
 import pandas as pd
 import requests
@@ -25,30 +25,24 @@ def fetch_video_data(query, max_results):
 # Function to fetch thumbnail images
 def fetch_thumbnail(url):
     response = requests.get(url)
-    img_data = response.content
-    img = Image.open(BytesIO(img_data))
-    thumbnail = ImageTk.PhotoImage(img)
+    thumbnail = Image.open(BytesIO(response.content))
+    thumbnail = thumbnail.resize((120, 90), Image.ANTIALIAS)
+    thumbnail = ImageTk.PhotoImage(thumbnail)
     return thumbnail
 
-# Function to analyze video data and apply the filter
-def analyze_video_data(videos, filter_option):
-    df = pd.DataFrame(columns=['Title', 'Views', 'Thumbnail', 'Link'])
+# Function to analyze video data
+def analyze_video_data(videos):
+    df = pd.DataFrame(columns=['Title', 'Views', 'Thumbnail', 'Link', 'Selected'])
     for video in videos:
         title = video['snippet']['title']
         views = int(video['statistics']['viewCount'])
         thumbnail_url = video['snippet']['thumbnails']['medium']['url']
         video_id = video['id']['videoId']
         thumbnail = fetch_thumbnail(thumbnail_url)
-        df = df.append({'Title': title, 'Views': views, 'Thumbnail': thumbnail, 'Link': f'https://www.youtube.com/watch?v={video_id}'}, ignore_index=True)
-
-    if filter_option == 'alphabetically':
-        df = df.sort_values('Title')
-    elif filter_option == 'by views':
-        df = df.sort_values('Views', ascending=False)
-
+        df = df.append({'Title': title, 'Views': views, 'Thumbnail': thumbnail, 'Link': f'https://www.youtube.com/watch?v={video_id}', 'Selected': False}, ignore_index=True)
     return df
 
-# Function to display the GUI with the video list
+# Function to display the GUI
 def display_video_list(df, page_num):
     start_index = (page_num - 1) * 100
     end_index = start_index + 100
@@ -64,60 +58,78 @@ def display_video_list(df, page_num):
     # Apply filter button
     def apply_filter():
         filter_option = filter_combobox.get()
-        filtered_df = analyze_video_data(videos, filter_option)
-        display_video_list(filtered_df, 1)
+        df_filtered = analyze_video_data(videos, filter_option)
+        display_video_list(df_filtered, 1)
 
-    filter_button = ttk.Button(root, text='Apply Filter', command=apply_filter)
-    filter_button.pack(pady=5)
+    apply_filter_btn = ttk.Button(root, text='Apply Filter', command=apply_filter)
+    apply_filter_btn.pack(pady=10)
 
-    # Create scrollable list
-    frame = tk.Frame(root)
-    frame.pack(pady=10)
-    canvas = tk.Canvas(frame, width=800, height=600)
-    canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-    scrollbar = ttk.Scrollbar(frame, orient=tk.VERTICAL, command=canvas.yview)
-    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-    canvas.configure(yscrollcommand=scrollbar.set)
-    canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox('all')))
+    # Create canvas for video list
+    canvas = tk.Canvas(root)
+    canvas.pack(fill='both', expand=True)
+
+    # Create inner frame for video list
     inner_frame = tk.Frame(canvas)
-    canvas.create_window((0, 0), window=inner_frame, anchor='nw')
+    inner_frame.pack()
 
+    # Create scrollbar for canvas
+    scrollbar = ttk.Scrollbar(root, orient='vertical', command=canvas.yview)
+    scrollbar.pack(side='right', fill='y')
+
+    # Configure canvas and scrollbar
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.create_window((0, 0), window=inner_frame, anchor='nw')
+    inner_frame.bind('<Configure>', lambda event: canvas.configure(scrollregion=canvas.bbox('all')))
+
+    # Add checkbox for each video
     for i, (_, row) in enumerate(df[start_index:end_index].iterrows(), start=start_index+1):
         title = row['Title']
         thumbnail = row['Thumbnail']
         link = row['Link']
 
-        # Create hyperlink label
-        hyperlink = tk.Label(inner_frame, text=title, fg='blue', cursor='hand2')
-        hyperlink.grid(row=i, column=0, padx=10, pady=5, sticky='w')
-        hyperlink.bind('<Button-1>', lambda event, l=link: root.clipboard_append(l))
+        checkbox = tk.Checkbutton(inner_frame)
+        checkbox.grid(row=i, column=2)
 
-        # Create thumbnail image
         thumbnail_label = tk.Label(inner_frame, image=thumbnail)
-        thumbnail_label.grid(row=i, column=1, padx=10, pady=5)
+        thumbnail_label.grid(row=i, column=0, padx=10, pady=5)
 
-    # Page selector
-    def change_page():
-        selected_page = int(page_selector.get())
-        display_video_list(df, selected_page)
+        hyperlink = tk.Label(inner_frame, text=title, fg='blue', cursor='hand2')
+        hyperlink.grid(row=i, column=1, padx=10, pady=5)
+        hyperlink.bind('<Button-1>', lambda event, l=link: messagebox.showinfo('YouTube Video', l))
 
-    page_selector = tk.Spinbox(root, from_=1, to=len(df) // 100 + 1, width=5)
-    page_selector.delete(0, tk.END)
-    page_selector.insert(0, page_num)
-    page_selector.pack(pady=10)
-    page_selector.bind('<Return>', lambda event: change_page())
+    # Add Select All button
+    def select_all():
+        for i in df.index:
+            df.at[i, 'Selected'] = True
+        display_video_list(df, page_num)
+
+    select_all_btn = ttk.Button(root, text='Select All', command=select_all)
+    select_all_btn.pack(pady=10)
+
+    # Add Download Selected button
+    def download_selected():
+        selected = df[df['Selected'] == True]
+        # Download selected videos
+
+    download_selected_btn = ttk.Button(root, text='Download Selected', command=download_selected)
+    download_selected_btn.pack(pady=10)
+
+    # Add Download All button
+    def download_all():
+        # Download all videos
+
+    download_all_btn = ttk.Button(root, text='Download All', command=download_all)
+    download_all_btn.pack(pady=10)
 
     root.mainloop()
 
-
-# Fetch video data from YouTube
+# Fetch video data
 query = 'data analysis tutorials'
 max_results = 500
 videos = fetch_video_data(query, max_results)
 
-# Analyze video data and apply filter
-filter_option = 'by views'
-df = analyze_video_data(videos, filter_option)
+# Analyze video data
+df = analyze_video_data(videos)
 
 # Display video list
 display_video_list(df, 1)
